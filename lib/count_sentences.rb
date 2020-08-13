0ad1e7abf805440989951128fca5f44af6b7be99
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-13 21:02:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,9 @@ class String
   end
 
   def count_sentences
-    self.split(/[!?.]/).reject { |sentences| sentences = "" || sentences = " " }.length
+    self.split(/[!?.]/)
+    self.reject { |sentences| sentences = "" || sentences = " " }
+    self.length
   end
 end
 #binding.pry
